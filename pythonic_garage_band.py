1772fcd4db75f760bcec4713084fb1e97d875364
nastinsk <nastinsk@gmail.com>
2019-12-05 21:37:22 -0800

    Musician class created

--- pythonic_garage_band.py
+++ pythonic_garage_band.py
@@ -1,9 +1,12 @@
 # base class or parent class
 class Musician:
 
+    members = []
+
     def __init__(self, role, instrument):
         self.role = role
         self.instrument = instrument
+        self.__class__.members.append(self)
 
     def __str__(self):
         return f'I am a {self.role}'
@@ -12,26 +15,52 @@ class Musician:
         return f'This is {self.role}'
 
     def play_solo(self):
-        return f'{self.role} is playing on the {self.instrument}'
+        return f'{self.role} is playing solo on the {self.instrument}'
 
     def get_instrument(self):
         return self.instrument
 
 
-
 class Guitarist(Musician):
     pass
 
-guitarist = Guitarist("Guitarist", "guitar")
+class Bassist(Musician):
+    pass
+
+class Drummer(Musician):
+    pass
+
+guitarist = Guitarist('Guitarist', 'guitar')
+bassist = Bassist('Bassist','bass')
+drummer = Drummer('Drummer', 'drums')
+drummer2 = Drummer('Drummer', 'drums')
 
 
 
+class Band:
 
-# class Guitarist(Musician)
-# class Guitarist(Musician)
+    all = []
 
-#create musian class
-# describe role, str,repr, play_solo(), get_instrument() - return string,
-# create object instances that made with this class
-#
+    def __init__(self, name, members):
+        self.name = name
+        self.members = members
+        self.__class__.alt.append(self)
+
+
+    # def play_solos():
+
+
+    # def __str__():
+
+    # def __repr__():
+
+    @classmethod
+    def to_list(cls):
+        return all
+
+    # @staticmethod
+    # def create_from_data()
+
+band = Band('kazaks', Musician.members)
+print(Band.name)
 
--- test_pythonic_garage_band.py
+++ test_pythonic_garage_band.py
@@ -1,13 +1,40 @@
-from pythonic_garage_band import Musician, Guitarist
+from pythonic_garage_band import Musician, Guitarist, Drummer, Bassist
 
 def test_guitarist_role():
-    expected = "Guitarist"
-    actual = Musician('Guitarist', 'guitar').role
+    expected = 'Guitarist'
+    actual = Guitarist('Guitarist', 'guitar').role
     assert expected == actual
 
 def test_guitarist_instrument():
-    expected = "guitar"
-    actual = Musician('Guitarist', 'guitar').instrument
+    expected = 'guitar'
+    actual = Guitarist('Guitarist', 'guitar').instrument
     assert expected == actual
+
+def test_guitarist_str():
+    expected = 'I am a Guitarist'
+    actual = Guitarist('Guitarist', 'guitar').__str__()
+    assert expected == actual
+
+def test_guitarist_repr():
+    assert 'This is Guitarist' == Guitarist('Guitarist', 'guitar').__repr__()
+
+def test_guitarist_get_instrument():
+    assert 'guitar' == Guitarist('Guitarist', 'guitar').get_instrument()
+
+def test_guitarist_play_solo():
+    assert 'Guitarist is playing solo on the guitar' == Guitarist('Guitarist', 'guitar').play_solo()
+
+def test_drummer_play_solo():
+     assert 'Drummer is playing solo on the drums' == Drummer('Drummer', 'drums').play_solo()
+
+def test_guitarist_str():
+    assert 'I am a Bassist' == Bassist('Bassist', 'bass').__str__()
+
+# def test_musicians_members():
+#     Musician.members = []
+#     guitarist = Guitarist('Guitarist', 'guitar')
+#     bassist = Bassist('Bassist','bass')
+#     drummer = Drummer('Drummer', 'drums')
+#     assert [guitarist, bassist, drummer] == Musician.members
 
 
